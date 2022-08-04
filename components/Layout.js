61ae5a28183a77e4ed8ac25d0d3bfd345aebf74b
Erik Truckner <75428920+ABSURD0@users.noreply.github.com>
2022-08-04 16:19:18 -0400

    Add files via upload
     Create Login API
        1. install next-auth
        2. create nextauth.js
        3. implement signin
        4. use signin in login form

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,14 @@
+import { useSession } from 'next-auth/react'
 import Head from 'next/head'
 import Link from 'next/link'
 import React, { useContext, useEffect, useState } from 'react'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import { Store } from '../utils/Store'
 
 export default function Layout({ title, children }) {
+  const { status, data: session } = useSession()
+
   const { state } = useContext(Store)
   const { cart } = state
   const [cartItemsCount, setCartItemsCount] = useState(0)
@@ -13,15 +18,18 @@ export default function Layout({ title, children }) {
   return (
     <>
       <Head>
-        <title>{title ? title + ' - AbsurDesign' : 'AbsurDesign'}</title>
+        <title>{title ? title + ' - Amazona' : 'Amazona'}</title>
         <meta name='description' content='Ecommerce Website' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
+
+      <ToastContainer position='bottom-center' limit={1} />
+
       <div className='flex min-h-screen flex-col justify-between '>
         <header>
           <nav className='flex h-12 items-center px-4 justify-between shadow-md'>
             <Link href='/'>
-              <a className='text-lg font-bold'>AbsurDesign</a>
+              <a className='text-lg font-bold'>amazona</a>
             </Link>
             <div>
               <Link href='/cart'>
@@ -34,15 +42,22 @@ export default function Layout({ title, children }) {
                   )}
                 </a>
               </Link>
-              <Link href='/login'>
-                <a className='p-2'>Login</a>
-              </Link>
+
+              {status === 'loading' ? (
+                'Loading'
+              ) : session?.user ? (
+                session.user.name
+              ) : (
+                <Link href='/login'>
+                  <a className='p-2'>Login</a>
+                </Link>
+              )}
             </div>
           </nav>
         </header>
         <main className='container m-auto mt-4 px-4'>{children}</main>
         <footer className='flex h-10 justify-center items-center shadow-inner'>
-          <p>Copyright © 2022 AbsurDesign</p>
+          <p>Copyright © 2022 Amazona</p>
         </footer>
       </div>
     </>

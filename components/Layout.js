b0388f5caafae188c23e5a900b85ba0ec05fa87e
Erik Truckner <75428920+ErikTruckner@users.noreply.github.com>
2022-08-08 17:15:16 -0400

    Add files via upload

--- components/Layout.js
+++ components/Layout.js
@@ -27,7 +27,7 @@ export default function Layout({ title, children }) {
   return (
     <>
       <Head>
-        <title>{title ? title + ' - Amazona' : 'Amazona'}</title>
+        <title>{title ? title + ' - AbsurDesign' : 'AbsurDesign'}</title>
         <meta name='description' content='Ecommerce Website' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
@@ -36,7 +36,7 @@ export default function Layout({ title, children }) {
         <header>
           <nav className='flex h-12 items-center px-4 justify-between shadow-md'>
             <Link href='/'>
-              <a className='text-lg font-bold'>amazona</a>
+              <a className='text-lg font-bold'>AbsurDesign</a>
             </Link>
             <div>
               <Link href='/cart'>
@@ -89,7 +89,7 @@ export default function Layout({ title, children }) {
         </header>
         <main className='container m-auto mt-4 px-4'>{children}</main>
         <footer className='flex h-10 justify-center items-center shadow-inner'>
-          <p>Copyright © 2022 Amazona</p>
+          <p>Copyright © 2022 AbsurDesign</p>
         </footer>
       </div>
     </>
